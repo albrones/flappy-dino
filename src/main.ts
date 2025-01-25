ed9f558b0a444ea03857fd67a0928f38a02919ec
Alexis B <abrones1@gmail.com>
2025-01-25 19:51:54 +0100

    ✨ Make character selection random on load

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { k } from './kaplayLoader';
 import { initGameScene } from './scenes/game';
 import { initMenuScoreScene } from './scenes/menu-score';
 
+export const characterList = ['bean', 'monster-1'];
+let playerSprite = k.choose(characterList);
 //create main parent
 k.add([pos(0, 0)]);
 k.scene('menu-score', (score, playerSprite) => {
@@ -10,4 +12,4 @@ k.scene('menu-score', (score, playerSprite) => {
 k.scene('game', (playerSprite) => {
   initGameScene(k, playerSprite);
 });
-k.go('menu-score', 0, 'bean');
+k.go('menu-score', 0, playerSprite);
